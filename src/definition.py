db84f45e4c849648aa4a5d5c69f3e4d51b7f5c18
Erwin DIDE <didymedide0808@gmail.com>
2026-04-15 10:29:03 -0700

    Definition du job pour créer toutes les tables a partir du fichier src/definition.py

--- src/definition.py
+++ src/definition.py
@@ -1,9 +1,17 @@
-from dagster import Definitions, load_assets_from_package_module
+from dagster import Definitions, load_assets_from_package_module, AssetSelection
 from src import assets  #on importe le dossier asset
 
 # on charge tous les assets qui sont dans notre dossier src/assets/
 tous_les_assets = load_assets_from_package_module(assets)
 
+#on fait pareil pour le job
+chargement_tables_job = define_asset_job(
+    name="chargement_tables_job",
+    selection=AssetSelection.all() #on prend tous nos assets
+)
+
 defs = Definitions(
     assets=tous_les_assets,
+    jobs=[chargement_tables_job]
 )
+
