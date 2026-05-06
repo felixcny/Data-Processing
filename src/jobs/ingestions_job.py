d62243d6405e10837b0977990a1d864b5c89c23a
Erwin DIDE <didymedide0808@gmail.com>
2026-05-06 01:54:49 -0700

    Mise a jour jobs Dagster

--- src/jobs/ingestions_job.py
+++ src/jobs/ingestions_job.py
@@ -1,7 +1,24 @@
 from dagster import define_asset_job, AssetSelection
 
+
+maselection = AssetSelection.keys(
+
+    "commentaires_json",
+    "paniers_json",
+    "produits_json",
+    "publications_json",
+    "recettes_json",
+    "utilisateurs_json",
+    
+    "table_commentaires",
+    "table_paniers",
+    "table_produits",
+    "table_publications",
+    "table_recettes",
+    "table_utilisateurs"
+)
 #on définit le job qui charge toutes nos données ici
 chargement_tables_job = define_asset_job(
     name="chargement_tables_job",
-    selection=AssetSelection.all()
+    selection=maselection
 )
